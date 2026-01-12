3c3403334c8ef210487ed8648ad81183089de105
shwetaojha710 <shweta@quaeretech.com>
2026-01-12 19:01:36 +0530

    commit

--- src/app/branchwise/branchwise.component.ts
+++ src/app/branchwise/branchwise.component.ts
@@ -19,16 +19,30 @@ export class BranchwiseComponent {
     branchList: any
     obj: any = {};
     stats:any = []
+    User:any={}
   constructor(private dashboardService: DashboardService, private router: Router, public masterService: MasterService) {
      this.getBranchDD()
+     this.User = JSON.parse(localStorage.getItem('tenant') || '{}');
   }
+  DummyBranchData=[
+  {
+    "id": "4abc445c-01e7-40dd-9439-6a5ce8b2a8f6",
+    "name": "XYZ",
+    "description": "Central administration and monitoring access."
+  }
+]
   getBranchDD(){
     this.branchList=[]
     this.masterService.BranchDD().subscribe((res) => {
       if (res.status == true) {
         this.notyf.success(res.message || 'Dashboard data loaded successfully')
         this.stats = res.data.stats;
+        if(res.data.length==0){
+          this.branchList=this.DummyBranchData
+        }else{
         this.branchList = res.data
+        }
+
       } else if (res.status == 'expired') {
         this.router.navigate(['login'])
       } else {
@@ -43,4 +57,7 @@ export class BranchwiseComponent {
   // redirect to dashboard
   this.router.navigate(['/layout/dashboard']);
 }
+
+
+
 }
